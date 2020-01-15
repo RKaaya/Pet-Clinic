057d1ef29eff6c5eb4785d61e8f65775e14c49c8
RKaaya <gergelynagy99@gmail.com>
2020-01-15 22:46:27 +0100

    adding POJOs.

---
+++ src/main/java/rkaaya/springframework/petclinic/model/Pet.java
@@ -0,0 +1,15 @@
+package rkaaya.springframework.petclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDay;
+}
---
+++ src/main/java/rkaaya/springframework/petclinic/model/PetType.java
@@ -0,0 +1,11 @@
+package rkaaya.springframework.petclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PetType {
+
+    private String name;
+}
